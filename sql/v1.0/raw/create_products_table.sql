59028a7d5d591d4adbedfa87290bbd6e090e609e
Datanaut <108269078+MarikoTheeCityZen@users.noreply.github.com>
2026-08-12 15:43:44 +0300

    Create products table in raw schema
    Created a products table with relevant metadata including product details and load information.

---
+++ sql/v1.0/raw/create_products_table.sql
@@ -0,0 +1,23 @@
+use ECommerceWH;
+go 
+
+--Create products table with relevant metadata
+
+create table raw.products
+(
+product_code varchar(50),
+product_name nvarchar(50),
+brand nvarchar(50),
+subcategory_name nvarchar(50),
+unit_price decimal(10,2),
+primary_supplier nvarchar(50),
+product_description nvarchar(1000),
+hash_key nvarchar(100),
+source_id varchar(50),
+--metadata
+_load_id bigint not null,
+_loaded_at datetime2(3) not null 
+constraint df_raw_products_loaded_at 
+default sysutcdatetime(),
+_source_object nvarchar(50) not null
+);

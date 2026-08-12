32c70958d660204bb115951844bb2a3c088e9428
Datanaut <108269078+MarikoTheeCityZen@users.noreply.github.com>
2026-08-12 11:34:52 +0300

    Create cust_master table for raw customer data
    Created a table to store customer data with metadata.

--- sql/v1.0/raw/create_cust_master_table.sql
+++ sql/v1.0/raw/create_cust_master_table.sql
@@ -1,1 +1,21 @@
+use ECommerceWH;
+go
 
+--Creating customers table to hold raw data as ingested from our source with metadata
+
+create table raw.cust_master
+(
+customer_id int  null,
+customer_name nvarchar(100) null,
+segment nvarchar(20) null,
+address_id int null,
+account_manager nvarchar(20) null,
+hash_key varchar null,
+source_id nvarchar(20) null,
+--metadata
+_load_id bigint not null,
+_loaded_at datetime2(3) 
+constraint raw_cust_master_loaded_at 
+default sysutcdatetime(),
+_source_object nvarchar(50) not null
+);

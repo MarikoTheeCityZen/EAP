07941c6595990afe6397e5f038b3c41f04b7ab47
Datanaut <108269078+MarikoTheeCityZen@users.noreply.github.com>
2026-08-12 11:53:39 +0300

    Create customer_contacts table in raw schema
    Create customer_contacts table with metadata fields.

--- sql/v1.0/raw/create_customer_contacts_table.sql
+++ sql/v1.0/raw/create_customer_contacts_table.sql
@@ -1,1 +1,19 @@
+use ECommerceWH;
+go 
+
+--Create customer contacts table with the necessary metadata
+
+create table raw.customer_contacts
+(
+customer_id int,
+contact_name nvarchar(30),
+email nvarchar(50),
+is_primary nvarchar(10),
+--metadata
+_load_id bigint not null,
+_loaded_at datetime2(3) not null 
+constraint df_raw_cust_contacts_loaded_at 
+default sysutcdatetime(),
+_source_object nvarchar(50) not null
+);
 
